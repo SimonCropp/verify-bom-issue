37bfcab5570990805760143df2aa4a5503b2478c
Simon Cropp <simon.cropp@gmail.com>
2023-12-23 11:44:49 +1100

    verify no bom

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace verify_bom_issue;
 
 [UsesVerify]
@@ -6,6 +8,12 @@ public class UnitTest1
     [Fact]
     public async void Test1()
     {
-        await Verify("test");
+        var file = CurrentFile.Relative("UnitTest1.Test1.verified.txt");
+        var encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: true);
+        var target = "test";
+        await Verify(target);
+        var fileBytes = await File.ReadAllBytesAsync(file);
+        var expectedBytes = encoding.GetBytes(target);
+        Assert.Equal(expectedBytes, fileBytes);
     }
 }
